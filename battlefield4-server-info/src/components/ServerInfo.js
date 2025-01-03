99e39a0e96d965b9a6d7a77b156f0f6396112f53
Pranjal Jain <jainpranjal30112002@gmail.com>
2025-01-03 18:55:18 +0530

    fetch from backend

--- battlefield4-server-info/src/components/ServerInfo.js
+++ battlefield4-server-info/src/components/ServerInfo.js
@@ -1,37 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import ServerDetails from './ServerDetails';
 import Stats from './Stats';
 import Button from './Button';
-export default function ServerInfo  () {
+
+export default function ServerInfo() {
   const [serverData, setServerData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
-      .get('http://localhost:5000/api/server-info')
+      .get('https://battlefield4-monorepo-backend.vercel.app/server-info')
       .then((response) => {
         setServerData(response.data);
       })
-      .catch((error) => console.error('Error fetching data:', error));
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setError('Failed to load server data');
+      });
   }, []);
 
+  if (error) return <div>{error}</div>;
   if (!serverData) return <div>Loading...</div>;
 
   return (
     <div className="server-info">
       <h1>{serverData.title}</h1>
       <p>{serverData.description}</p>
-      
       <div className="buttons-container">
         <Button text="Join" />
         <Button text="Spectate" />
         <Button text="Join as Commander" />
       </div>
-      
-      {/* {serverData.serverDetails && (
-        <ServerDetails data={serverData.serverDetails} />
-      )} */}
       <Stats />
     </div>
   );
-};
+}
